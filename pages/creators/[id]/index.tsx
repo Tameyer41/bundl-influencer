5b25d98fe0fe0bc92ebe682722b72cccaa888ff8
Tameyer41 <63265436+Tameyer41@users.noreply.github.com>
2022-04-14 17:26:09 -0500

    v6

--- pages/creators/[id]/index.tsx
+++ pages/creators/[id]/index.tsx
@@ -1,52 +1,7 @@
 import React from "react";
-import { prisma } from "@lib/prisma";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 
-export async function getStaticProps({ params }) {
-  // fetch single post detail
-  const response = await fetch(
-    `https://dreamy-dragon-1e86de.netlify.app/api/users/${params.id}`
-  );
-  const user = await response.json();
-  return {
-    props: user,
-    revalidate: 60,
-  };
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const users = await fetch(
-    "https://dreamy-dragon-1e86de.netlify.app/api/users"
-  ).then((response) => response.json());
-
-  const ids = users.map((user) => user.id);
-  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-const User = (props) => {
-  const { data: session, status } = useSession();
-  if (status === "loading") {
-    return <div>Authenticating ...</div>;
-  }
-  let name = props.name;
-
-  const router = useRouter();
-
-  return router.isFallback ? (
-    <h1>Loading...</h1>
-  ) : (
-    <div>
-      <h2>{name}</h2>
-      <p>{props.email}</p>
-    </div>
-  );
+const User = () => {
+  return <h1> Test</h1>;
 };
 
 export default User;
